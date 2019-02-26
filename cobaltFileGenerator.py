df85c8c8db0a7b8994c8515c36aee94f9eed36c0
mac <luka.bubalo@gamersject.com>
2019-02-26 14:28:21 +0100

    Slight refactor
    Thanks to leovujanic for suggestions!

--- cobaltFileGenerator.py
+++ cobaltFileGenerator.py
@@ -1,43 +1,54 @@
-import os, sys
+import os
+import sys
 from docx import Document
 
-directoryName = "generated-test-files"
-defaultPack = ['jpg', 'jpeg', 'pdf', 'png', 'docx', 'gif', 'doc']
-magicNumbers = {
-	"jpg" : bytearray.fromhex("FF D8 FF DB"),
-	"jpeg" : bytearray.fromhex("FF D8 FF EE"),
-	"pdf" : bytearray.fromhex("25 50 44 46 2d"),
-	"png" : bytearray.fromhex("89 50 4E 47 0D 0A 1A 0A"),
-	"docx" : bytearray.fromhex("50 4B 03 04 14 00 00 00 08 00 6F 6E 74 65 6E 74 5F 54 79"),
-	"doc" : bytearray.fromhex("D0 CF 11 E0 A1 B1 1A E1"),
-	"gif" : bytearray.fromhex("47 49 46 38 37 61")
+output_dir = "generated-test-files"
+MAGIC_NUMBERS = {
+    "jpg": bytearray.fromhex("FF D8 FF DB"),
+    "jpeg": bytearray.fromhex("FF D8 FF EE"),
+    "pdf": bytearray.fromhex("25 50 44 46 2d"),
+    "png": bytearray.fromhex("89 50 4E 47 0D 0A 1A 0A"),
+    "docx": bytearray.fromhex(
+        "50 4B 03 04 14 00 00 00 08 00 6F 6E 74 65 6E 74 5F 54 79"),
+    "doc": bytearray.fromhex("D0 CF 11 E0 A1 B1 1A E1"),
+    "gif": bytearray.fromhex("47 49 46 38 37 61")
 }
 
-def createFile(extension):
-	global directoryName
-	global magicNumbers
-	fileName = directoryName + '/' + extension + 'TestFile.' + extension
-	if(extension == "docx" or extension == "doc"):
-		document = Document()
-		document.add_heading('Test Heading', 0)
-		document.add_paragraph('Test Paragraph')
-		document.save(fileName)
-	else:
-		open(fileName, 'a').close()
-		f = open(fileName, mode='w+b')
-		f.write(magicNumbers[extension])
-		f.close()
+def _create_doc(file_name):
+    document = Document()
+    document.add_heading("Test Heading", 0)
+    document.add_paragraph("Test Paragraph")
+    document.save(file_name)
 
-if os.path.exists(directoryName):
-	os.system('rm -rf '+ directoryName)
 
-os.mkdir(directoryName)
+def create_file(file_ext, out_dir):
+    if ext not in MAGIC_NUMBERS.keys():
+        raise ValueError("Unsupported extension {}".format(file_ext))
 
-if len(sys.argv) > 1:
-	for extension in sys.argv:
-		if extension in magicNumbers:
-			createFile(extension)
-else: ## if there are no args provided generate files from defaultPack
-	for extension in defaultPack:
-		createFile(extension)
+    file_name = "{0}/{1}TestFile.{2}".format(out_dir, file_ext, file_ext)
 
+    if ext in ("docx", "doc"):
+        _create_doc(file_name)
+        return
+
+    with open(file_name, "wb") as fh:
+        fh.write(MAGIC_NUMBERS[ext])
+
+
+def ensure_output_dir(out_dir):
+    if os.path.exists(out_dir):
+        os.system("rm -rf {}".format(out_dir))
+
+    os.mkdir(out_dir)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        extensions = sys.argv[1:]
+    else:
+        extensions = MAGIC_NUMBERS.keys()
+
+    ensure_output_dir(output_dir)
+
+    for ext in extensions:
+        create_file(ext, output_dir)
